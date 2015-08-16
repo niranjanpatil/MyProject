3e1de11105f2fa8033fa4ac62c7e10fb0d8b2318
niranjanpatil <p.niranjan.d@gmail.com>
2015-08-16 05:20:47 -0500

    Adding bugfix code

--- src/main/java/TestClass2.java
+++ src/main/java/TestClass2.java
@@ -5,6 +5,6 @@ public class TestClass2 {
 
     public static void main(String[] args) {
 
-        System.out.println("Class ceated in development branch");
+        System.out.println("Class ceated in development branch. Making change in bugfix branch");
     }
 }
