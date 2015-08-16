5edcf3f51407c7e0972b9f4093047fac4a0fd31b
niranjanpatil <p.niranjan.d@gmail.com>
2015-08-16 04:48:45 -0500

    Adding Test Class2

---
+++ src/main/java/TestClass2.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Niranjan on 8/16/2015.
+ */
+public class TestClass2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Class ceated in development branch");
+    }
+}
