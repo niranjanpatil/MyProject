69d115ff7539e84eb4c7b3a2576b4b5d384cb9b7
niranjanpatil <p.niranjan.d@gmail.com>
2015-08-16 04:44:03 -0500

    Initial commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Niranjan on 8/16/2015.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test class");
+    }
+}
